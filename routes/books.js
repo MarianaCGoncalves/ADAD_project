8f32eafd909bdc7f1a1912fda73d28d42c1b86f1
MariaMVhs <mmvso@iscte-iul.pt>
2024-10-31 17:33:44 +0000

    ponto 6 +/-, ponto 5 erros

--- routes/books.js
+++ routes/books.js
@@ -20,5 +20,41 @@ router.get('/', (req, res) => {
     });
   });
 
+  router.get('/:id', async (req, res) => {
+    const id = parseInt(req.params.id);
+    let results = await db.collection('books').aggregate([
+      {$match: {_id: id}} , 
+      {$lookup: {from: "users",
+                let:{_id: id},
+                pipeline: [
+                  {$unwind: "$reviews"},
+                  {$match: {book_id: id} },
+                  {$group: {_id: "$reviews.book_id"},
+                  average_score: {$avg: "$reviews.score"}}
+                ],
+                as: "average_score"
+                }
+      },
+      {$lookup: {from: "comments",
+                 localField: "_id",
+                 foreignField: "book_id",
+                 as: "book_comments"
+      }},
+      {$addFields: {
+        average_score: "$average_score",
+        book_comments: "$book_comments"
+      }}
+    ]).toArray();
+
+    if (!results || results.length === 0) { 
+      res.status(404).send("Book not found");
+   } else { 
+      res.status(200).send(results); }
+  });
+
+  router.get('/:id', async (req, res) => {
+    const id = parseInt(req.params.id);
+    let results = await db.collection('books').find({_id: id})
+
 export default router;
 
--- routes/users.js
+++ routes/users.js
@@ -10,4 +10,19 @@ router.get("/", async (req, res) => {
     res.send(results).status(200);
 });
 
+router.get("/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+    let results = await db.collection('users').aggregate([
+        {$match: {_id: id}},
+        {$unwind: "$reviews"},
+         {$sort: {score: -1}},
+        {$limit: 3}
+     ])
+    .toArray();
+    if (!results || results.length === 0) { 
+        res.status(404).send("User not found");
+     } else { 
+        res.status(200).send(results); }
+    });
+
 export default router;
