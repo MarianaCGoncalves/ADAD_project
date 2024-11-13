3b12341108252febef44be2168d17527407404c6
rmfga <rmfga@iscte-iul.pt>
2024-11-13 11:21:12 +0000

    Correção
    Tava com uns stresses no meu config e app

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 import express from 'express';
-import movies from "./routes/movies.js";
+import books from "./routes/books.js";
+import users from "./routes/users.js";
+import comments from "./routes/comments.js";
+import livrarias from "./routes/livrarias.js";
 
-const app = express()
-const port = 3000
-app.get('/', (req, res) => {
-res.send('Backend!')
-})
+const app = express();
+const port = 3000;
 
 app.use(express.json());
-// Load the /movies routes
-app.use("/movies", movies);
 
+// Load the routes
+app.use("/books", books);
+app.use("/users", users);
+app.use("/comments", comments);
+app.use("/livrarias", livrarias);
+
+// Remove 'require' and directly use the already imported 'books' route
+//app.use('/api', books);
+console.log("Rota livrarias carregada");  // Adiciona esta linha para verificar se a rota é carregada
 app.listen(port, () => {
-    console.log(`backend listening on port ${port}`)
-    })
+  console.log(`Backend listening on port ${port}`);
+});
